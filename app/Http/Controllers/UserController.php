ebe9ba1efa521f8695f51ea801c6a32b7777051d
hakizimana-fred <hakifred20@gmail.com>
2021-02-13 20:43:52 +0300

    interracting with DB

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function index()
     {
+        DB::insert("INSERT INTO users(name, email, password) values(?,?,?)", [
+            "fred", "fred@gmail.com", "password"
+        ]);
         return view('user');
     }
 }
